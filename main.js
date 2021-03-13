5019b5e5a8c76f3b46455ad3520b43646ab29b8c
ucarlos1001 <ucarlos1001@gmail.com>
2021-03-13 10:21:41 -0300

    Gerador de senha

---
+++ main.js
@@ -0,0 +1,34 @@
+const showPassowrd = document.getElementById('show')
+const buttonCopy = document.getElementById('button-copy')
+
+document.getElementById('gerar-senha').addEventListener('click', event => {
+    event.preventDefault()
+
+    var sizePassowrd = document.getElementById('size-password').value
+    var errorArea = document.getElementById('error-area')
+
+    if (sizePassowrd !== '0') {
+        const carac = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:[]{}."
+        var password = ""
+
+        for (var i = 0; i < sizePassowrd; i++) {
+            var randomNumber = Math.floor(Math.random() * carac.length)
+            password += carac.substring(randomNumber, randomNumber + 1)
+        }
+
+        showPassowrd.value = password
+        errorArea.innerHTML = ''
+        buttonCopy.style.display = 'block'
+    } else {
+        errorArea.innerHTML = 'É necessário selecionar o tamanho da senha!'
+        buttonCopy.style.display = 'none'
+    }
+
+})
+
+function copyPassword(content) {
+    content.select()
+    content.setSelectionRange(0, 99999)
+    document.execCommand("copy")
+}
+
