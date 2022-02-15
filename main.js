7b5c964acdee5e308b2188bfce52ff83d9f85360
ucarlos1001 <ucarlos1001@gmail.com>
2022-02-15 19:36:49 -0300

    Finalizado

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const buttonCopy = document.getElementById('button-copy')
 document.getElementById('gerar-senha').addEventListener('click', event => {
     event.preventDefault()
 
-    var sizePassowrd = document.getElementById('size-password').value
-    var errorArea = document.getElementById('error-area')
+    let sizePassowrd = document.getElementById('size-password').value
+    let errorArea = document.getElementById('error-area')
 
     if (sizePassowrd !== '0') {
-        const carac = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:[]{}."
-        var password = ""
+        const carac = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:[]{}"
+        let password = ""
 
-        for (var i = 0; i < sizePassowrd; i++) {
-            var randomNumber = Math.floor(Math.random() * carac.length)
+        for (let i = 0; i < sizePassowrd; i++) {
+            let randomNumber = Math.floor(Math.random() * carac.length)
             password += carac.substring(randomNumber, randomNumber + 1)
         }
 
@@ -31,4 +31,3 @@ function copyPassword(content) {
     content.setSelectionRange(0, 99999)
     document.execCommand("copy")
 }
-
